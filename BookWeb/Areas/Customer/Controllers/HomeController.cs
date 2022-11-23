8b41e3bf30e6f228a7f4d45891165851cc4ce0e9
Mukhammadaziz Juraev <juraev.mukhammadaziz@gmail.com>
2022-11-23 11:46:40 +0500

    Added UnitOFWork

--- BookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using BookWeb.Models;
+﻿using BookWeb.DataAccess.Repository.IRepository;
+using BookWeb.Models;
+using BookWeb.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace BookWeb.Controllers
@@ -8,15 +11,29 @@ namespace BookWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            ShoppingCart cartObj = new()
+            {
+                Count = 1,
+                Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType")
+            };
+
+            return View(cartObj);
         }
 
         public IActionResult Privacy()
